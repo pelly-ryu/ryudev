3022a5863ca69643207618045534f2cac9e1d74a
pelly ryu <pelly.ryu@gmail.com>
2021-11-02 09:36:58 +0900

    delete useless script

--- script.js
+++
@@ -1,5 +0,0 @@
-const name = document.querySelector("wired-input");
-const button = document.querySelector("wired-button");
-button.addEventListener("click", () => {
-  window.alert(`Hello ${name.value.trim()}!`);
-});
